8ba4be72d2241bff88064c36ad505c41701c2584
sungsanghwa <sangwha0@naver.com>
2025-12-17 21:30:18 +0900

    chore(config): add webconfig file

---
+++ src/main/java/com/studyCommunity/Community/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.studyCommunity.Community.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new UserHeaderInterceptor());
+    }
+
+    //cors도 설정 해보기
+}
