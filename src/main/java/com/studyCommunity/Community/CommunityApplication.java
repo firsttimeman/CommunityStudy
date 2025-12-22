9100c34caf97d55768153f60b032b9805a545c2b
sungsanghwa <sangwha0@naver.com>
2025-12-22 13:36:48 +0900

    chore: fix configure

--- src/main/java/com/studyCommunity/Community/CommunityApplication.java
+++ src/main/java/com/studyCommunity/Community/CommunityApplication.java
@@ -9,13 +9,13 @@ public class CommunityApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv =  Dotenv.configure()
-				.directory("./") // .env 파일 경로 설정 (기본: 프로젝트 루트)
-				.load();
-
-		dotenv.entries().forEach(entry ->
-				System.setProperty(entry.getKey(), entry.getValue())
-		);
+//		Dotenv dotenv =  Dotenv.configure()
+//				.directory("./") // .env 파일 경로 설정 (기본: 프로젝트 루트)
+//				.load();
+//
+//		dotenv.entries().forEach(entry ->
+//				System.setProperty(entry.getKey(), entry.getValue())
+//		);
 
 		SpringApplication.run(CommunityApplication.class, args);
 	}
