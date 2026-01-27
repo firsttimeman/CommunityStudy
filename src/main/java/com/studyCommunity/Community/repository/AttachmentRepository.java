decb820fee11c87e3d3e2c1b34e1b90d9c57f163
sungsanghwa <sangwha0@naver.com>
2026-01-27 20:04:16 +0900

    feat: 만료시간 jpql 구성

--- src/main/java/com/studyCommunity/Community/repository/AttachmentRepository.java
+++ src/main/java/com/studyCommunity/Community/repository/AttachmentRepository.java
@@ -2,13 +2,32 @@ package com.studyCommunity.Community.repository;
 
 import com.studyCommunity.Community.entity.Attachment;
 import com.studyCommunity.Community.entity.Post;
+import com.studyCommunity.Community.type.AttachmentStatus;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
 
     List<Attachment> findAllByPost(Post post);
+
+    @Query("""
+            SELECT a
+            from Attachment a
+            where a.attachmentStatus = :status
+            and a.expireAt < :now
+            order by a.expireAt asc
+            """)
+    List<Attachment> findExpiredTempAttachments(
+            @Param("status") AttachmentStatus status,
+            @Param("now") LocalDateTime now,
+            Pageable pageable
+    );
+
 }
