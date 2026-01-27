455cd59a49b59932a6d4adeb15540c52d76ff294
sungsanghwa <sangwha0@naver.com>
2026-01-27 23:03:37 +0900

    feat: BaseResponse 초기

--- src/main/java/com/studyCommunity/Community/controller/BaseResponse.java
+++ src/main/java/com/studyCommunity/Community/controller/BaseResponse.java
@@ -1,2 +1,18 @@
-package com.studyCommunity.Community.controller;public class BaseResponse {
+package com.studyCommunity.Community.controller;
+
+//todo 다시 한번더 code에 대해서 물어보기
+public class BaseResponse<T> {
+    private final T result;
+    private final String message;
+    private final int code;
+
+    public BaseResponse(T result, String message, int code) {
+        this.result = result;
+        this.message = message;
+        this.code = code;
+    }
+
+    public static <T> BaseResponse<T> ok(T result, int code) {
+        return new BaseResponse<>(result, "OK", code);
+    }
 }
