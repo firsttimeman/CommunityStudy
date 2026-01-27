5d81d14e46a6f734d0ff2fa8c92efd96bfc32c35
sungsanghwa <sangwha0@naver.com>
2026-01-27 21:40:12 +0900

    chore: dotenv 설정변경으로 인한 간편화 및 스케쥴러 어노테이션 설정

--- src/main/java/com/studyCommunity/Community/CommunityApplication.java
+++ src/main/java/com/studyCommunity/Community/CommunityApplication.java
@@ -3,20 +3,13 @@ package com.studyCommunity.Community;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CommunityApplication {
 
 	public static void main(String[] args) {
-
-//		Dotenv dotenv =  Dotenv.configure()
-//				.directory("./") // .env 파일 경로 설정 (기본: 프로젝트 루트)
-//				.load();
-//
-//		dotenv.entries().forEach(entry ->
-//				System.setProperty(entry.getKey(), entry.getValue())
-//		);
-
 		SpringApplication.run(CommunityApplication.class, args);
 	}
 
