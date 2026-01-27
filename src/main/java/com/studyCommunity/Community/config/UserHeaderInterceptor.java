b5134667c35fc3055ee542ac25607ddf86c517ae
sungsanghwa <sangwha0@naver.com>
2026-01-27 19:29:58 +0900

    refactor: 컨벤션 수정

--- src/main/java/com/studyCommunity/Community/config/UserHeaderInterceptor.java
+++ src/main/java/com/studyCommunity/Community/config/UserHeaderInterceptor.java
@@ -3,17 +3,26 @@ package com.studyCommunity.Community.config;
 import com.studyCommunity.Community.exception.BadRequestException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+@Component
 public class UserHeaderInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String userId = request.getHeader("X-User-Id");
-        if(userId == null || userId.isBlank()) {
+        if (userId == null || userId.isBlank()) {
             throw new BadRequestException("X-User-Id header is required");
         }
         request.setAttribute("userId", userId);
         return true;
     }
+
+    /**
+     인터셉터로 매번 컨트롤러에서 요청을 보내면 번거로울 뿐만 아니라 만약 x-user-id형식이 아닌 다른 형식으로 변경될 경우
+     편하게 변경이 가능
+     *
+     */
+
 }
--- src/main/java/com/studyCommunity/Community/config/WebConfig.java
+++ src/main/java/com/studyCommunity/Community/config/WebConfig.java
@@ -1,15 +1,19 @@
 package com.studyCommunity.Community.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final UserHeaderInterceptor userHeaderInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new UserHeaderInterceptor());
+        registry.addInterceptor(userHeaderInterceptor);
     }
 
     //cors도 설정 해보기
