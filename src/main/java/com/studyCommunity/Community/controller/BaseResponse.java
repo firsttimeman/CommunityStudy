051228891d218f184ddf3f2674ad607f1e52957d
sungsanghwa <sangwha0@naver.com>
2026-01-27 23:03:37 +0900

    검
    # Conflicts:
    #	src/main/java/com/studyCommunity/Community/controller/BaseResponse.java

--- src/main/java/com/studyCommunity/Community/controller/BaseResponse.java
+++ src/main/java/com/studyCommunity/Community/controller/BaseResponse.java
@@ -1,18 +1,23 @@
 package com.studyCommunity.Community.controller;
 
-//todo 다시 한번더 code에 대해서 물어보기
+import org.springframework.http.HttpStatus;
+
 public class BaseResponse<T> {
     private final T result;
     private final String message;
     private final int code;
 
-    public BaseResponse(T result, String message, int code) {
+    private BaseResponse(T result, HttpStatus status) {
         this.result = result;
-        this.message = message;
-        this.code = code;
+        this.message = status.getReasonPhrase();
+        this.code = status.value();
     }
 
-    public static <T> BaseResponse<T> ok(T result, int code) {
-        return new BaseResponse<>(result, "OK", code);
+    public static <T> BaseResponse<T> of(T result, HttpStatus status) {
+        return new BaseResponse<>(result, status);
+    }
+
+    public static BaseResponse<Void> of(HttpStatus status) {
+        return new BaseResponse<>(null, status);
     }
 }
