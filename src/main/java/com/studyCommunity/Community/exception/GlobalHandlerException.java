da0707d6b922de869f8f9d5cffd3e790568cb9e9
sungsanghwa <sangwha0@naver.com>
2026-02-20 19:26:51 +0900

    feat: globalexception 500 로그 수정

--- src/main/java/com/studyCommunity/Community/exception/GlobalHandlerException.java
+++ src/main/java/com/studyCommunity/Community/exception/GlobalHandlerException.java
@@ -1,6 +1,7 @@
 package com.studyCommunity.Community.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalHandlerException {
 
     @ExceptionHandler(CustomException.class)
@@ -37,6 +39,7 @@ public class GlobalHandlerException {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleEtc(Exception e, HttpServletRequest req) {
+        log.error("Unhandled error {} {}", req.getMethod(), req.getRequestURI(), e);
         return ResponseEntity.status(INTERNAL_SERVER_ERROR)
                 .body(ErrorResponse.of(500, "INTERNAL_SERVER_ERROR", "Unexpected error", req.getRequestURI()));
     }
