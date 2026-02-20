e788bf000fd5e1c1bbdb8c26ebfc624f4326dd64
sungsanghwa <sangwha0@naver.com>
2026-02-20 19:29:32 +0900

    feat: PostRepository 쿼리 최적화

--- src/main/java/com/studyCommunity/Community/repository/PostRepository.java
+++ src/main/java/com/studyCommunity/Community/repository/PostRepository.java
@@ -1,12 +1,15 @@
 package com.studyCommunity.Community.repository;
 
 import com.studyCommunity.Community.dto.PostListResponse;
+import com.studyCommunity.Community.entity.Attachment;
 import com.studyCommunity.Community.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
@@ -24,4 +27,36 @@ public interface PostRepository extends JpaRepository<Post, Long> {
             order by p.createTime desc
             """)
     Page<PostListResponse> findPostList(Pageable pageable);
+
+
+//    @Query("""
+//            select new com.studyCommunity.Community.dto.PostListResponse(
+//                p.postId,
+//                p.title,
+//                p.userId,
+//                p.createTime,
+//                COUNT(distinct c.commentId),
+//                COUNT(distinct a.attachmentId)
+//            )
+//            from Post p
+//            left join Comment c on c.post = p
+//            left join Attachment a on a.post = p
+//            group by p
+//            order by count(distinct c.commentId) desc
+//            """)
+//    Page<PostListResponse> findPopularPostList(Pageable pageable);
+
+
+    @Query("""
+  select new com.studyCommunity.Community.dto.PostListResponse(
+    p.postId, p.title, p.userId, p.createTime,
+    p.commentCount, p.attachmentCount
+  )
+  from Post p
+  order by p.commentCount desc, p.postId desc
+""")
+    Page<PostListResponse> findPopularPostList(Pageable pageable);
+
+
+
 }
