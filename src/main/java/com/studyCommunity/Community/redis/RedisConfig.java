13235ff5251377d21ec98bda3e3addcc3953e467
sungsanghwa <sangwha0@naver.com>
2026-03-02 22:47:07 +0900

    feat: Redis 환경설정 구현

---
+++ src/main/java/com/studyCommunity/Community/redis/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.studyCommunity.Community.redis;
+
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        String address = "redis://" + redisHost + ":" + redisPort;
+        Config config = new Config();
+        config.useSingleServer().setAddress(address);
+        return Redisson.create(config);
+    }
+}
